543eded66eb25c0987cd9a9caca65360aaaf7276
snagh <andrews.cs16@gmail.com>
2019-05-30 21:30:40 -0300

    Update main.c

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 
 int main(int argc, char *argv[]) {
 	
-	/* mpp: máximo de pessoas permitidas, qp: quantidade de pessoas dentro do elevador. op: opção
+	/* mpp: mÃ¡ximo de pessoas permitidas, qp: quantidade de pessoas dentro do elevador. op: opÃ§Ã£o
 	 porta: estado da porta (0: fechada, 1: aberta) , andar: andar atual do elevador, tandar: total de andares */
 	
 	int porta, mpp, andar, qp, tandar, op;
@@ -15,6 +15,7 @@ int main(int argc, char *argv[]) {
 	mpp = 10;
 	porta = 0;
 	qp = 0;
+	printf("O elevador esta no ANDAR: %d, a PORTA esta FECHADA e esta com uma carga de %d pessoas\n", andar, qp);
 	
 	while(op!=7)
 	{
@@ -25,7 +26,50 @@ int main(int argc, char *argv[]) {
 		{
 			printf("DIGITE O ANDAR DESEJADO: ");
 			scanf("%d",&andar);
+			if(andar==0)
+			{
+				printf(" O elevador ja esta no andar solicitado\n Va para o proximo passo.\n");
+			}
+			
 		}
+		if (op==2)
+		{	
+			porta=1;
+			printf("Porta aberta!\n");		
+		}
+		
+		
+		if(op==4)
+		{
+			if(porta==1)
+			{
+				printf("QUANTAS PESSOAS QUEREM ENTRAR? \n");
+				scanf("%d", &qp);
+				if(qp>mpp)
+				{
+					printf("NUMERO MAXIMO DE PESSOAS PERMITIDAS ULTRAPASSADO\n");
+				}
+			}
+			else
+			{
+				printf("ABRA A PORTA!");
+			}
+		}
+		
+		if(op==3)
+		{
+			porta=0;
+		}
+		
+		if(op==5)
+		{
+			if(porta==1)
+			{
+				qp=0;	
+			}
+						
+		}
+		
 	}
 	
 	
